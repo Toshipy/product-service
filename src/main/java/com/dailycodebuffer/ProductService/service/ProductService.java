4f51359ddd33f457ae58a04cb3de349b350b6f89
arai <arai@tagbangers.co.jp>
2023-11-12 19:20:34 +0900

    first commit

---
+++ src/main/java/com/dailycodebuffer/ProductService/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.dailycodebuffer.ProductService.service;
+
+import com.dailycodebuffer.ProductService.model.ProductRequest;
+
+public interface ProductService {
+
+    long addProduct(ProductRequest productRequest);
+}
